0d4f44db2de439c813ca697fbbbf7229b90be437
AthmaniAmine <a_athmani@estin.dz>
2025-01-26 17:22:08 +0100

    Update logout.js

--- project/controllers/logout.js
+++ project/controllers/logout.js
@@ -2,14 +2,13 @@ const jwt = require("jsonwebtoken");
 
 const logout = (req, res) => {
     
-    res.cookie('userSave', null, {
-        expires: new Date(0), 
-        httpOnly: false,
-        sameSite:'None',
-         secure:true ,
-         domain:"front-flame-psi.vercel.app",
-         path: '/'
-    });
+   res.clearCookie("userSave", {
+    httpOnly: true, // Secure cookie
+    sameSite: "None", // Cross-site cookies
+    secure: true, // HTTPS only
+    domain: "front-flame-psi.vercel.app", // Frontend domain
+    path: "/", // Path of the cookie
+});
     
     
     res.status(200).json({ status: "success", message: "User logout successfully" });
